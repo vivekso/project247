25d9cd03edf21ac51b7d964150d4af308db52d91
Vivek <sonkar.vivek@yahoo.com>
2023-06-18 19:25:48 +0530

    change in route

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 // We can get the profile by this Route for this we have to pass Bearer Token
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 // 1st method of routing 
 // Route::post('/signup', [SignupController::class,'signup']);
 // Route::post('/login', [SignupController::class,'login']);
